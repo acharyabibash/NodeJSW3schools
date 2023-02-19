9be8948a8776b6f62507d0196324d9af48408da7
acharyabibash <acharabibash@outlook.com>
2023-02-19 18:38:37 +0545

    Write,Overwrite and Append files in NodeJS

--- demo_readfile.js
+++ demo_readfile.js
@@ -7,3 +7,19 @@ http.createServer(function(req,res){
         return res.end();
     });
 }).listen(8080);
+
+
+fs.appendFile('mynewfile1.txt','Hello content!',function(err){
+    if(err) throw err;
+    console.log('Saved!');
+});
+
+fs.open('mynewfile2.txt','w',function(err,file){
+    if(err) throw err;
+    console.log('Saved!');
+});
+
+fs.writeFile('mynewfile3.txt','Hello content!',function(err){
+    if(err) throw err;
+    console.log('Saved!');
+});
